415b20009b111dbfaf9932f33e0f5943c26f215a
LefMono000 <a2-supremacy@outlook.com>
2024-09-06 01:15:45 -0600

    Updates

---
+++ Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Components/AvisosYNoticias.css';
+import AvisosYNoticias from './AvisosYNoticias.js';
+
+function App() {
+  return (
+    <div className="App">
+      <AvisosYNoticias />
+    </div>
+  );
+}
+
+export default App;
